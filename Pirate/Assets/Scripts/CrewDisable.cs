05391c1b57b29fccbadd3c582f7aaf07328a39a0
MehmetTurkerofRivia <mertmemo69@gmail.com>
2024-02-12 10:38:14 +0300

    aaa

--- Pirate/Assets/Scripts/CrewDisable.cs
+++ Pirate/Assets/Scripts/CrewDisable.cs
@@ -7,6 +7,7 @@ public class CrewDisable : MonoBehaviour
     [SerializeField] GameObject CrewShadowPanel;
     [SerializeField] GameObject CrewButtonDisable;
 
+<<<<<<< HEAD
     //void Update()
     //{
     //    if (pirateData != null && crewShadowPanel != null)
@@ -19,4 +20,17 @@ public class CrewDisable : MonoBehaviour
     //        CrewButtonDisable.SetActive(true);
     //    }
     //}
+=======
+    void Update()
+    {
+        if (CrewShadowPanel.activeInHierarchy)
+        {
+            CrewButtonDisable.SetActive(false);
+        }
+        else
+        {
+            CrewButtonDisable.SetActive(true);
+        }
+    }
+>>>>>>> parent of 1ed4594 (prefab yapıldı)
 }
